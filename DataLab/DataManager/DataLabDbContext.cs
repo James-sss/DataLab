00911ee27301326e7b55b8f50229dc320c676547
James-sss <53993050+James-sss@users.noreply.github.com>
2021-12-12 15:51:58 +0200

    DataLabDbContext.cs and ApplicationUsers.cs Added

---
+++ DataLab/DataManager/DataLabDbContext.cs
@@ -0,0 +1,30 @@
+﻿using FarmApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.DataManager
+{
+    public class DataLabDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public DataLabDbContext(DbContextOptions<DataLabDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+               .Property(u => u.FullName)
+               .HasComputedColumnSql("[FirstName] + ' ' + [LastName]");
+
+        }
+    }
+}
---
+++ DataLab/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmApplication.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; private set; }
+
+
+        //public virtual ICollection<FarmMembers> FarmMembers { get; set; }
+    }
+}
