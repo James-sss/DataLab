06b1a44f6e259c2273692f42df2485730e27ca4d
James-sss <53993050+James-sss@users.noreply.github.com>
2021-12-21 10:10:14 +0200

    RolesController, CreateView and backend

---
+++ DataLab/Controllers/RolesController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using DataLab.Models;
+using DataLab.ViewModels.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly INotyfService _toastNotification;
+
+        public RolesController(RoleManager<IdentityRole> roleManager,
+                                 UserManager<ApplicationUser> userManager,
+                                 INotyfService toastNotification)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _toastNotification = toastNotification;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult CreateUserRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUserRole(UserRolesVM model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    _toastNotification.Success($"Role {model.RoleName} Was Successfully Created ");
+                    return RedirectToAction("CreateUserRole");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ DataLab/ViewModels/Authorization/UserRolesVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.ViewModels.Authorization
+{
+    public class UserRolesVM
+    {
+        [Required(ErrorMessage = "Role Name is required")]
+        public string RoleName { get; set; }
+
+        //Return all List of roles
+        public IEnumerable<IdentityRole> IdentityRole { get; set; }
+    }
+}
