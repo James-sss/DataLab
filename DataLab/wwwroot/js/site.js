8a956d85da09dda9af5fa3ec195e904328da31b9
James-sss <53993050+James-sss@users.noreply.github.com>
2021-12-21 09:42:19 +0200

    Cleaning up redundant code

--- DataLab/wwwroot/js/site.js
+++ DataLab/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $("#table1").DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "autoWidth": false,
+        "buttons": false
+    });
+});
+
+$(document).ready(function () {
+    $("#table2").DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "autoWidth": false,
+        "buttons": false
+    });
+});
