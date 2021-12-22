9f6e21dcbd0661656121f72bfcdb8dbf3ce77a9d
James-sss <53993050+James-sss@users.noreply.github.com>
2021-12-22 23:06:38 +0200

    Started working on AuthorizedUsers

---
+++ DataLab/IServices/IAuthUsersService.cs
@@ -0,0 +1,16 @@
+﻿using DataLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.IServices
+{
+    public interface IAuthUsersService
+    {
+        bool IsUserInCompany(ApplicationUser user, int CustomerId);
+        Task<AuthorizedUsers> AddAuthUsers(AuthorizedUsers users);
+        Task<AuthorizedUsers> RemoveAuthUsers(AuthorizedUsers users);
+        IEnumerable<AuthorizedUsers> GetAuthUsersList(int CustomerId);
+    }
+}
---
+++ DataLab/Repositories/AuthUserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataLab.DataManager;
+using DataLab.IServices;
+using DataLab.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.Repositories
+{
+    public class AuthUserRepository : IAuthUsersService
+    {
+        private readonly DataLabDbContext _context;
+
+        public AuthUserRepository(DataLabDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AuthorizedUsers> AddAuthUsers(AuthorizedUsers users)
+        {
+            await _context.AuthorizedUsers.AddAsync(users);
+            await _context.SaveChangesAsync();
+            return users;
+        }
+
+        public IEnumerable<AuthorizedUsers> GetAuthUsersList(int CustomerId)
+        {
+            return _context.AuthorizedUsers.Where(u => u.CustomerId == CustomerId).Include(u => u.ApplicationUser).ToList();
+        }
+
+        public bool IsUserInCompany(ApplicationUser user, int CustomerId)
+        {
+            return _context.AuthorizedUsers.Any(e => e.ApplicationUser.Id == user.Id && e.CustomerId == CustomerId);
+        }
+
+        public async Task<AuthorizedUsers> RemoveAuthUsers(AuthorizedUsers users)
+        {
+            _context.AuthorizedUsers.RemoveRange(users);
+            await _context.SaveChangesAsync();
+            return users;
+        }
+    }
+}
