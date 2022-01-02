edf121330a2557bfdb2f4fe0994861c8ab057129
James-sss <53993050+James-sss@users.noreply.github.com>
2022-01-02 19:20:26 +0200

    GetCharts backend

--- DataLab/Controllers/ChartsController.cs
+++ DataLab/Controllers/ChartsController.cs
@@ -1,4 +1,5 @@
 ﻿using DataLab.IServices;
+using DataLab.ViewModels.Chart;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,8 +20,85 @@ namespace DataLab.Controllers
             _customerService = customerService;
         }
 
-        public IActionResult Index()
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCharts( int id)
         {
+            var customer = await _customerService.GetCustomerByid(id);
+
+            if (customer == null)
+            {
+                Response.StatusCode = 404;
+                return View("DashBordNotFoundErros");
+            }
+
+
+            ViewBag.CustomerId = customer.CustomerId;
+            ViewBag.CustomerName = customer.CustomerName;
+            ViewBag.DataSource = customer.DataSource;
+
+
+            string SensorTypelist;
+            string Monthlist;
+            string AverageValueslist;
+            string MinimumValueslist;
+            string MaxmumValueslist;
+            string TotalFilesCountlist;
+
+
+            var GetSensorDataResults = _dataService.GetAllDataByCustId(customer.CustomerId).Select(e => new
+            {
+                Datetime = e.Datetime.Value,
+                SensorTypes = e.SensorTypes.SensorType,
+                Value = e.Value
+
+            }).OrderBy(e => e.Datetime.Month).GroupBy(x => new { x.SensorTypes, month = x.Datetime.ToString("MMMM") }, (key, group) => new ChartVM
+            {
+                SensorType = key.SensorTypes,
+                Month = key.month,
+                AverageValues = group.Count() > 0 ? group.Average(x => x.Value) : 0,
+                MinimumValues = group.Min(x => x.Value),
+                MaxmumValues = group.Max(x => x.Value),
+                TotalFilesCount = group.Count(x => x.SensorTypes.Any())
+            }).OrderBy(e => e.SensorType).ToList();
+
+
+            List<ChartVM> SensorResultsList = new List<ChartVM>();
+
+            foreach (var CollectedSensorResult in GetSensorDataResults)
+            {
+                ChartVM SensorResults = new ChartVM();
+
+                SensorResults.SensorType = CollectedSensorResult.SensorType;
+                SensorResults.Month = CollectedSensorResult.Month;
+                SensorResults.AverageValues = (int)CollectedSensorResult.AverageValues;
+                SensorResults.MinimumValues = (int)CollectedSensorResult.MinimumValues;
+                SensorResults.MaxmumValues = (int)CollectedSensorResult.MaxmumValues;
+                SensorResults.TotalFilesCount = (int)CollectedSensorResult.TotalFilesCount;
+
+                SensorResultsList.Add(SensorResults);
+
+
+                SensorTypelist = "'" + string.Join("','", SensorResultsList.Select(n => n.SensorType).ToList()) + "'";
+                Monthlist = string.Join(", ", SensorResultsList.Select(n => n.Month).Distinct().ToList());
+                AverageValueslist = string.Join(",", SensorResultsList.Select(n => n.AverageValues).ToList());
+                MinimumValueslist = string.Join(",", SensorResultsList.Select(n => n.MinimumValues).ToList());
+                MaxmumValueslist = string.Join(",", SensorResultsList.Select(n => n.MaxmumValues).ToList());
+                TotalFilesCountlist = string.Join(",", SensorResultsList.Select(n => n.TotalFilesCount).ToList());
+
+
+                ViewBag.sensorTypes = SensorTypelist;
+                ViewBag.months = Monthlist;
+                ViewBag.AverageValues = AverageValueslist;
+                ViewBag.MinimumValues = MinimumValueslist;
+                ViewBag.MaxmumValues = MaxmumValueslist;
+                ViewBag.TotalDataFiles = TotalFilesCountlist;
+            }
+
+            //Testing results with json
+            //return Json(GetSensorDataResults);
+
             return View();
         }
     }
