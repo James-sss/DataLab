62ad16c020988298bcd7f93800267adaf8cc96cf
James-sss <53993050+James-sss@users.noreply.github.com>
2022-01-02 18:41:37 +0200

    Charts backend started

---
+++ DataLab/Controllers/ChartsController.cs
@@ -0,0 +1,27 @@
+﻿using DataLab.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.Controllers
+{
+    public class ChartsController : Controller
+    {
+        private readonly IDataService _dataService;
+        private readonly ICustomerService _customerService;
+
+        public ChartsController(IDataService dataService,
+                                ICustomerService customerService)
+        {
+            _dataService = dataService;
+            _customerService = customerService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataLab/ViewModels/Chart/ChartVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataLab.ViewModels.Chart
+{
+    public class ChartVM
+    {
+        public string Month { get; set; }
+        public string SensorType { get; set; }
+        public decimal AverageValues { get; set; }
+        public decimal MinimumValues { get; set; }
+        public decimal MaxmumValues { get; set; }
+        public int TotalFilesCount { get; set; }
+    }
+}
